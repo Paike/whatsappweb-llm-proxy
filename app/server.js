e140fa692ad5a71be85592c9f43291e97c803667
paike <github@pradikow.de>
2024-08-26 13:22:54 +0200

    added logging timestamp and catch for client initialization

--- app/server.js
+++ app/server.js
@@ -1,3 +1,5 @@
+require("log-timestamp");
+
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 // const express = require("express");
@@ -9,6 +11,9 @@ const logger = pino({
   level: "info", // Set the desired logging level
   transport: {
     target: "pino-pretty", // Use pino-pretty for human-readable output
+    options: {
+      translateTime: "SYS:yyyy-mm-dd'T'HH:MM:ss.l'Z'",
+    },
   },
 });
 
@@ -22,23 +27,6 @@ logger.debug("BACKEND_API_SCHEME: " + BACKEND_API_SCHEME);
 logger.debug("BACKEND_API_HOST: " + BACKEND_API_HOST);
 logger.debug("BACKEND_API_PORT: " + BACKEND_API_PORT);
 logger.info("BACKEND_API_URL: " + BACKEND_API_URL);
-
-// wwebjs configuration
-const client = new Client({
-  puppeteer: {
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  },
-  authStrategy: new LocalAuth({ dataPath: "data/.wwebjs_auth/" }),
-  webVersionCache: { path: "data/.wwebjs_cache/" },
-});
-
-let receivedQr = null;
-let clientInitialized = false;
-let chatId = null;
-let wid = null;
-
-// health check backend
-
 
 const checkHealth = async () => {
   try {
@@ -65,29 +53,46 @@ const checkHealth = async () => {
     return false;
   }
 };
+let receivedQr = null;
+let clientInitialized = false;
+let chatId = null;
+let wid = null;
+let client;
 
+// wwebjs configuration
+try {
+  client = new Client({
+    puppeteer: {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
+    authStrategy: new LocalAuth({ dataPath: "data/.wwebjs_auth/" }),
+    webVersionCache: { path: "data/.wwebjs_cache/" },
+  });
 
+  // show qr code in console
+  client.on("qr", (qr) => {
+    logger.info(`Received QR-code: ${qr}`);
+    receivedQr = qr;
+    qrcode.generate(qr, { small: true });
+  });
 
-// show qr code in console
-client.on("qr", (qr) => {
-  logger.info(`Received QR-code: ${qr}`);
-  receivedQr = qr;
-  qrcode.generate(qr, { small: true });
-});
+  client.on("ready", () => {
+    clientInitialized = true;
+    logger.info("Client is ready");
+    try {
+      wid = client.info.wid._serialized; // Get the serialized ID of the logged-in user
+      logger.info(`Logged in as ${wid}`);
+    } catch (error) {
+      logger.error(`Error getting account info: ${error}`);
+    }
+    checkHealth();
+  });
+  client.initialize();
+} catch (error) {
+  console.error("Failed to initialize the client:", error);
+  return;
+}
 
-client.on("ready", () => {
-  clientInitialized = true;
-  logger.info("Client is ready");
-  try {
-    wid = client.info.wid._serialized; // Get the serialized ID of the logged-in user
-    logger.info(`Logged in as ${wid}`);
-  } catch (error) {
-    logger.error(`Error getting account info: ${error}`);
-  }
-  checkHealth();
-});
-
-client.initialize();
 
 client.on("message_create", async (message) => {
   chatId = message.from;
@@ -106,9 +111,10 @@ client.on("message_create", async (message) => {
   // Prepare lastMessage object
   const lastMessage = messagesData[messagesData.length - 1]; // Get the last message = the new message
 
-   // Filter out previous messages with the same body as the new message
-   const history = messagesData.slice(0, messagesData.length - 1) // Exclude the last message
-   .filter(msg => msg.body !== lastMessage.body);
+  // Filter out previous messages with the same body as the new message
+  const history = messagesData
+    .slice(0, messagesData.length - 1) // Exclude the last message
+    .filter((msg) => msg.body !== lastMessage.body);
 
   // Prepare payload
   const payload = {
@@ -116,12 +122,12 @@ client.on("message_create", async (message) => {
     history: history,
     lastMessage: lastMessage,
   };
-  logger.info({payload}, "Payload");
-  
+  logger.info({ payload }, "Payload");
+
   // Send data to python backend
   try {
     logger.info("Sending message to backend");
-    const response = await axios.post(BACKEND_API_URL + "/inference", git p);
+    const response = await axios.post(BACKEND_API_URL + "/inference", payload);
     client.sendMessage(message.from, response.data);
     logger.info(`Received from backend: ${response.data}`);
   } catch (error) {
